c7515a1fd0048c693dccd055a6aec7859360b538
Nathan Pagnucco <nathan.p3pro@gmail.com>
2025-01-17 17:28:00 +0100

    Version 0.3 - Manuel du module fonctionnel
    J'ai rajouté le manuel du module et il représente bien les différents symboles à afficher et les numéros associés.

--- Assets/Scripts/UI/Modules/MathSymbolRulesElement.cs
+++ Assets/Scripts/UI/Modules/MathSymbolRulesElement.cs
@@ -1,13 +1,37 @@
+using UnityEngine;
 using UnityEngine.UIElements;
 
 [UxmlElement]
 public partial class MathSymbolRulesElement : VisualElement
 {
-
+    private VisualElement TabSymbols => this.Q<VisualElement>("tabSymbols");
 
     public void Init(MathSymbolRuleGenerator generator)
     {
+        Sprite[] symbolsSprites = generator.GetSymbolsSprites();
+        Symbols[,] symbolsRep = generator.GetSymbolsRepartition();
+        int[] buttonValues = generator.GetButtonValues();
 
+        for (int i = 0; i < 8; i++)
+        {
+            Label numHolder = TabSymbols.Q<Label>($"num{i + 1}");
+            VisualElement symbolHolder = TabSymbols.Q<VisualElement>($"symbols{i + 1}");
+
+            numHolder.text = buttonValues[i].ToString();
+            for (int y = 0; y < 5; y++)
+            {
+                VisualElement symbol = new();
+                symbol.style.backgroundImage = symbolsSprites[(int)symbolsRep[i, y]].texture;
+                symbol.style.width = 64;
+                symbol.style.height = 64;
+                //symbol.style.maxHeight = 64;
+                //symbol.style.maxWidth = 64;
+                //symbol.style.minHeight = 64;
+                //symbol.style.minWidth = 64;
+                symbolHolder.Add(symbol);
+
+            }
+        }
     }
 
     public MathSymbolRulesElement() { }
